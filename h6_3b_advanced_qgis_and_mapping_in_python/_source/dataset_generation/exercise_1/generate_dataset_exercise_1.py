6e39eb51606ecfc0bcc9cd4da1d1390e3210d5f8
Bergam0t <samanthajaderosser@gmail.com>
2024-04-04 16:13:24 +0100

    Update datasets to fix rate calculations

--- h6_3b_advanced_qgis_and_mapping_in_python/_source/dataset_generation/exercise_1/generate_dataset_exercise_1.py
+++ h6_3b_advanced_qgis_and_mapping_in_python/_source/dataset_generation/exercise_1/generate_dataset_exercise_1.py
@@ -5,9 +5,18 @@ msoa_gdf = geopandas.read_file("h6_3b_advanced_qgis_and_mapping_in_python/_sourc
 
 cols_to_calc_rate_list =  ['fatal_casualties_2018_2022',
        'cyclist_casualties_2018_2022', 'pedestrian_casualties_2018_2022',
-       'horse_rider_casualties_2018_2022', 'car_occupant_casualties_2018_2022']
+       'horse_rider_casualties_2018_2022', 'car_occupant_casualties_2018_2022', 'motorcycle_casualties_2018_2022']
 
 for col in cols_to_calc_rate_list:
+    new_colname = f"{col}_rate"
+    msoa_gdf[new_colname] = msoa_gdf.apply(lambda row: (row[col] / row['total_number_of_casualties_by_msoa_of_accident_2018_2022']), axis=1)
+
+cols_to_calc_rate_coll_list =  ['snow_collisions_2018_2022',
+       'fog_or_mist_collisions_2018_2022', 'darkness_collisions_2018_2022',
+       'daylight_collisions_2018_2022', 'summer_collisions_2018_2022',
+       'winter_collisions_2018_2022']
+
+for col in cols_to_calc_rate_coll_list:
     new_colname = f"{col}_rate"
     msoa_gdf[new_colname] = msoa_gdf.apply(lambda row: (row[col] / row['total_number_of_collisions_2018_2022']), axis=1)
 
@@ -19,9 +28,12 @@ msoa_pop_ests_2020 = pd.read_excel("h6_3b_advanced_qgis_and_mapping_in_python/_s
 msoa_gdf_pop = pd.merge(left=msoa_gdf, right=msoa_pop_ests_2020, left_on="MSOA11CD", right_on="MSOA Code", how="left")
 
 
-cols_to_calc_list =  ['total_number_of_casualties_2018_2022', 'fatal_casualties_2018_2022',
+cols_to_calc_list =  ['total_number_of_casualties_by_casualty_lsoa_2018_2022', 'fatal_casualties_2018_2022',
        'cyclist_casualties_2018_2022', 'pedestrian_casualties_2018_2022',
-       'horse_rider_casualties_2018_2022', 'car_occupant_casualties_2018_2022']
+       'horse_rider_casualties_2018_2022', 'car_occupant_casualties_2018_2022', 'snow_collisions_2018_2022',
+       'fog_or_mist_collisions_2018_2022', 'darkness_collisions_2018_2022',
+       'daylight_collisions_2018_2022', 'summer_collisions_2018_2022',
+       'winter_collisions_2018_2022']
 
 for col in cols_to_calc_list:
     new_colname = f"{col}_per_1000_occ"
